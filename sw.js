0e87bed18f7b5df27a46bfee1e042d766abd8c60
csdvywy962-cloud <csdvywy962@privaterelay.appleid.com>
2026-07-03 15:34:18 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,4 @@
-const CACHE_NAME = 'teleport-maps-v1';
-const ASSETS = [
-    '/',
-    '/index.html',
-    '/map.html',
-    '/style.css',
-    '/js.js' // Добавили новый файл в кэш!
-];
-
-self.addEventListener('install', (event) => {
-    event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
-});
-
+const CACHE_NAME = 'teleport-v1';
 self.addEventListener('fetch', (event) => {
     event.respondWith(caches.match(event.request).then((res) => res || fetch(event.request)));
 });
